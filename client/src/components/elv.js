a815f76dacc809fb8a1b9a0cd015c0c1d823d83b
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-21 15:54:07 -0400

    testing elv

---
+++ client/src/components/elv.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ElvWalletFrameClient } from "@eluvio/elv-wallet-frame-client";
+
+const Elv = (props) => {
+    return (
+      <div className="card" style={{padding: "50px"}}>
+        <img src={props.imageUrl} alt={props.imageAlt} style={{borderRadius: "20px", border: "solid", borderColor: "#ABDCD6", display: "block", margin: "0 auto", borderWidth: "10px", maxWidth: "60%"}} />
+        <div className="card-content">
+          <h2 style={{paddingTop: "20px", textAlign: "center", display: "block", margin: "0 auto"}}>{props.title}</h2>
+          <p style={{padding: "20px", textAlign: "center", display: "block", margin: "0 auto"}}>{props.description}</p>
+          <a href={props.buttonUrl}><button style={{textAlign: "center", display: "block", margin: "0 auto"}}>{props.buttonText}</button></a>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Elv;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import "./Home.css";
-// import Sarah from '../../images/IMG_1517.jpg';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { ElvWalletFrameClient } from "@eluvio/elv-wallet-frame-client";
 
+// Initialize in iframe at target element
+const walletFrameClient = await ElvWalletFrameClient.InitializeFrame({
+    requestor: "VSN",
+    walletAppUrl: "https://wallet.contentfabric.io",
+    target: document.getElementById("#wallet-target")
+  });
 
 function Home () {
     return (
         <main className="MainContainer" style={{padding: '10px'}}>
         <section className={`p-3`}>
-            <h2>Welcome to Tuter!</h2>
-            {/* return <image={{Sarah}}/> */}
-            <img src="/img/tuterlogo.png" style={{ display: "block", width: '300px', margin: "0 auto" }} />
-            <p className='mainText'>With Tuter App, you can easily connect with tuters online and send them any questions or notes you have. 
-            <br></br>
-            As a bonus, you can meet online face to face to discuss your homework at any time! 
-            <br></br>
-            Click the Tuters tab to connect today!</p>
+            <h2>Welcome to VSN!</h2>
+            <div id="wallet-target"></div>
         </section>
         </main>
     )
