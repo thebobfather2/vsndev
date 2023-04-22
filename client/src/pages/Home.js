3720ff2ae393dbd4108fbe61a918f6fffb9b3c91
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-21 20:36:17 -0400

    popup working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import "./Home.css";
 import { ElvWalletFrameClient } from "@eluvio/elv-wallet-frame-client";
+    
+// initialize in a popup
+async function initializeWalletFrameClient() {
+  const walletFrameClient = await ElvWalletFrameClient.InitializePopup({
+    requestor: "My App",
+    walletAppUrl: "https://wallet.contentfabric.io",
+  });
+  return walletFrameClient;
+}
 
-// Initialize in iframe at target element
-async function init() {
-    const walletFrameClient = await ElvWalletFrameClient.InitializeFrame({
-      requestor: "My App",
-      walletAppUrl: "https://wallet.contentfabric.io",
-      target: document.getElementById("#wallet-target")
-    });
-  }
-  
-  init();
+initializeWalletFrameClient()
+  .then((walletFrameClient) => {
+    // Use the walletFrameClient object here
+    console.log(walletFrameClient);
+  })
+  .catch((error) => {
+    // Handle any errors here
+    console.error(error);
+  });
 
 function Home () {
     return (
         <main className="MainContainer" style={{padding: '10px'}}>
         <section className={`p-3`}>
             <h2>Welcome to VSN!</h2>
-            <div id="wallet-target"></div>
         </section>
         </main>
     )
